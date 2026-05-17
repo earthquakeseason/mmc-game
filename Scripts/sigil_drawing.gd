ae50383b9b1aa4c0df3d549f822a36ea00e7964d
earthquakeseason <2vwilkin@hbhs.school.nz>
2026-05-18 10:59:26 +1200

    broke everything

--- Scripts/sigil_drawing.gd
+++ Scripts/sigil_drawing.gd
@@ -5,8 +5,7 @@ const FLAME_POINT_CLOUD: Array[Vector2i] = [Vector2i(165.0, 48.0), Vector2i(134.
 Vector2i(133.0, 104.0), Vector2i(84.0, 192.0), Vector2i(95.0, 132.0)]
 var image: Image
 var canvas_texture: ImageTexture
-var mouse_motions: Array[Vector2i]
-var distances: Array[float]
+var mouse_motions: Array[Gesture]
 
 func _ready() -> void:
 	GameEvents.submit_pressed.connect(_on_submit_pressed)
@@ -19,59 +18,57 @@ func _ready() -> void:
 func paint_texture(pos: Vector2i, paint_color: Color) -> void:
 	image.fill_rect(Rect2i(pos, Vector2i(1,1)).grow(3), paint_color)
 
-func _input(event: InputEvent) -> void:
-	if event is InputEventMouseButton:
+func _input(event: InputEvent) -> void:	
+	if event is InputEventMouseButton or event is InputEventMouseMotion:
+		var start_pos: Vector2i
+		var end_pos: Vector2i
+		var between_pos: Array[Vector2i]
 		if Input.is_action_just_pressed("left_click") or Input.is_action_just_pressed("right_click"):
 			var pos = to_local(event.position)
 			var impos = pos + get_rect().size / 2.0
 			if Input.is_action_just_pressed("left_click"):
 				paint_texture(impos, Color.BLACK)
-				add_new_point(impos)
+				start_pos = impos
 			else:
 				paint_texture(impos, Color.WHITE)
 			canvas_texture.update(image)
-	elif event is InputEventMouseMotion:
-		if Input.is_action_pressed("left_click") or Input.is_action_pressed("right_click"):
-			var chosen_color: Color
-			var pos: Vector2 = to_local(event.position)
-			var impos: Vector2 = pos + get_rect().size / 2.0
-			if Input.is_action_pressed("left_click"):
-				chosen_color = Color.BLACK
-				add_new_point(impos)
+		elif event is InputEventMouseMotion:
+			if Input.is_action_pressed("left_click") or Input.is_action_pressed("right_click"):
+				var chosen_color: Color
+				var pos: Vector2 = to_local(event.position)
+				var impos: Vector2 = pos + get_rect().size / 2.0
+				if Input.is_action_pressed("left_click"):
+					chosen_color = Color.BLACK
+				else:
+					chosen_color = Color.WHITE
+				paint_texture(impos, chosen_color)
+				canvas_texture.update(image)
+				if event.relative.length_squared() > 0:
+					var num = ceili(event.relative.length())
+					var target_pos = impos - (event.relative)
+					for i in num:
+						impos = impos.move_toward(target_pos, 1)
+						paint_texture(impos, chosen_color)
+						if i < num:
+							between_pos.append(impos)
+						else:
+							end_pos = impos
+		elif event.is_released():
+			print(between_pos)
+			if between_pos.is_empty():
+				mouse_motions.append(Gesture.new(start_pos, Vector2i(0, 0), [], true))
 			else:
-				chosen_color = Color.WHITE
-			paint_texture(impos, chosen_color)
-			canvas_texture.update(image)
-			if event.relative.length_squared() > 0:
-				var num = ceili(event.relative.length())
-				var target_pos = impos - (event.relative)
-				for i in num:
-					impos = impos.move_toward(target_pos, 1)
-					paint_texture(impos, chosen_color)
-	elif event.is_action("ui_up"):
+				mouse_motions.append(Gesture.new(start_pos, end_pos, between_pos, false))
+
+
+
+	elif event.is_action_pressed("ui_up"):
 		for mouse_motion in mouse_motions:
-			paint_texture(mouse_motion, Color.RED)
+			var random_color = Color(randf(), randf(), randf())
+			paint_texture(mouse_motion.start_pos, random_color)
+			if mouse_motion.single_point == false:
+				paint_texture(mouse_motion.end_pos, random_color)
 		canvas_texture.update(image)
 
-# most inefficient code EVER, ill fix this later i promise
-# not to mention this would detect a fully black screen as perfect
-#
-# resampling logic:
-# loop through points and sum the distances between each consecutive pair.
-# divide the total length by (N - 1)
-# start at the first point. move along the segments of the drawing.
-# every interval distance travelled drop a new point. if an interval ends between two original points. use lerp to find the exact spot.
-# use acos somewhere
-
 func _on_submit_pressed():
-	var total_length: float
-	for i in mouse_motions.size() - 1:
-		var distance_between_dots: float = mouse_motions[i].distance_to(mouse_motions[i + 1])
-		distances.append(distance_between_dots)
-		total_length += distance_between_dots
-	
-	print(total_length)
-
-func add_new_point(position: Vector2):
-	if mouse_motions.is_empty() or mouse_motions.back().distance_to(position) > 20:
-		mouse_motions.append(position)
+	pass
