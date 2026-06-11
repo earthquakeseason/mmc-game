7e660eda03916b6c815a49bbbd25ade790d529da
earthquakeseason <2vwilkin@hbhs.school.nz>
2026-06-11 14:07:24 +1200

    jucing...

--- scripts/global/ingredients.gd
+++ scripts/global/ingredients.gd
@@ -4,13 +4,14 @@ var all_ingredients: Array[Ingredient]
 
 func _ready() -> void:
 	# https://docs.godotengine.org/en/stable/classes/class_diraccess.html
-	var dir = DirAccess.open("res://resources/ingredients/")
+	const ACCESSED_PATH: String = "res://resources/ingredients/"
+	var dir = DirAccess.open(ACCESSED_PATH)
 	if dir:
 		dir.list_dir_begin()
 		var file_name = dir.get_next()
 		while file_name != "":
 			if not dir.current_is_dir() and file_name.split(".", false)[1] == "tres":
-				all_ingredients.append(load("res://resources/ingredients/" + file_name))
+				all_ingredients.append(load(ACCESSED_PATH + file_name))
 			file_name = dir.get_next()
 	else:
 		print("An error occurred when trying to access the path.")
--- scripts/global/potions.gd
+++ scripts/global/potions.gd
@@ -1,7 +1,17 @@
 extends Node
 
-func _ready() -> void:
-	pass # Replace with function body.
+var all_potions: Array[Potion]
 
-func _process(delta: float) -> void:
-	pass
+func _ready() -> void:
+	# https://docs.godotengine.org/en/stable/classes/class_diraccess.html
+	const ACCESSED_PATH: String = "res://resources/potions/"
+	var dir = DirAccess.open(ACCESSED_PATH)
+	if dir:
+		dir.list_dir_begin()
+		var file_name = dir.get_next()
+		while file_name != "":
+			if not dir.current_is_dir() and file_name.split(".", false)[1] == "tres":
+				all_potions.append(load(ACCESSED_PATH + file_name))
+			file_name = dir.get_next()
+	else:
+		print("An error occurred when trying to access the path.")
